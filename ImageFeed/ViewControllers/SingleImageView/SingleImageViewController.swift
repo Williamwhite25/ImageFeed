db9fb8ce7bed3a0f0565bae477f5c41b6314d9cc
WhiteCoconat <promo.for@yandex.ru>
2025-09-08 13:51:56 +0800

    Поправил картинку для центрирования

--- ImageFeed/ViewControllers/SingleImageView/SingleImageViewController.swift
+++ ImageFeed/ViewControllers/SingleImageView/SingleImageViewController.swift
@@ -60,8 +60,8 @@ final class SingleImageViewController: UIViewController {
         scrollView.setZoomScale(scale, animated: false)
         scrollView.layoutIfNeeded()
         let newContentSize = scrollView.contentSize
-        let x = (newContentSize.width - visibleRectSize.width)
-        let y = (newContentSize.height - visibleRectSize.height)
+        let x = (newContentSize.width - visibleRectSize.width) / 2
+        let y = (newContentSize.height - visibleRectSize.height) / 2
         scrollView.setContentOffset(CGPoint(x: x, y: y), animated: false)
     }
 }
