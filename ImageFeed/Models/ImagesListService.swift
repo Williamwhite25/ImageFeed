c9446dc749f61a7cbe46637d9a13780b2b375a2d
WhiteCoconat <promo.for@yandex.ru>
2025-10-10 19:53:45 +0800

    Тесты работают

--- ImageFeed/Models/ImagesListService.swift
+++ ImageFeed/Models/ImagesListService.swift
@@ -9,7 +9,7 @@ final class ImagesListService {
     static let didChangeNotification = Notification.Name(rawValue: "ImagesListServiceDidChange")
     
     var onPhotosUpdated: (() -> Void)?
-
+    
     func fetchPhotosNextPage() {
         guard !isLoading else { return }
         isLoading = true
@@ -60,65 +60,3 @@ final class ImagesListService {
 
 
 
-//
-//import Foundation
-//
-//final class ImagesListService {
-//    private(set) var photos: [Photo] = [] // Массив для хранения загруженных фотографий
-//    private var lastLoadedPage: Int? // Номер последней загруженной страницы
-//    private var isLoading = false // Флаг для проверки, идет ли сейчас загрузка
-//    
-//    // Замыкание для обновления UI
-//    var onPhotosUpdated: (() -> Void)?
-//
-//    // Функция для получения следующей страницы фотографий
-//    func fetchPhotosNextPage() {
-//        // Проверяем, идет ли сейчас загрузка
-//        guard !isLoading else {
-//            return // Если загрузка идет, выходим из функции
-//        }
-//
-//        isLoading = true // Устанавливаем флаг загрузки
-//
-//        // Определяем номер следующей страницы
-//        let nextPage = (lastLoadedPage ?? 0) + 1
-//        
-//        // Пример сетевого запроса (замените на ваш метод загрузки)
-//        let urlString = "https://example.com/photos?page=\(nextPage)"
-//        guard let url = URL(string: urlString) else {
-//            isLoading = false
-//            return // Если не удалось создать URL, выходим из функции
-//        }
-//
-//        // Выполняем запрос
-//        let task = URLSession.shared.dataTask(with: url) { [weak self] data, response, error in
-//            defer { self?.isLoading = false } // Сбрасываем флаг после завершения запроса
-//            
-//            // Обработка ошибок
-//            if let error = error {
-//                print("Error fetching photos: \(error)")
-//                return
-//            }
-//
-//            guard let data = data else {
-//                return // Если нет данных, выходим
-//            }
-//
-//            // Декодируем полученные данные в массив Photo
-//            do {
-//                let decoder = JSONDecoder()
-//                let photoResponse = try decoder.decode([Photo].self, from: data)
-//                self?.photos.append(contentsOf: photoResponse) // Добавляем новые фотографии в массив
-//                self?.lastLoadedPage = nextPage // Обновляем номер последней загруженной страницы
-//
-//                // Обновляем UI на главном потоке
-//                DispatchQueue.main.async {
-//                    self?.onPhotosUpdated?() // Вызываем замыкание для обновления UI
-//                }
-//            } catch {
-//                print("Error decoding photos: \(error)")
-//            }
-//        }
-//        task.resume() // Запускаем запрос
-//    }
-//}
